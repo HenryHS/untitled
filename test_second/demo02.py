e0c1cada2c267c282fba2a2f31e6f30e7ada0c9d
HenryHS <530869724@qq.com>
2019-06-27 10:47:54 +0800

    矩阵3种创建方式

--- test_second/demo02.py
+++ test_second/demo02.py
@@ -1,2 +1,16 @@
-from test.demo01 import *
+import numpy as np
+import random
 
+t1 = np.arange(12)
+
+l = [[1,2,3],[4,5,6],[7,8,'123']]
+
+t2 = np.array(l)
+
+t3 = np.random.randint()
+
+print(t3, type(t3), t3.shape,t3.dtype)
+
+t1 = t1.reshape(3, 4)
+
+# print(t1.dtype, t1.shape, t1.size, t1.itemsize)
